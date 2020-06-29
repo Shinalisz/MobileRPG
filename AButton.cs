553d8ae883691c8421100ae64648d2705340d609
Shinalisz <65959200+Shinalisz@users.noreply.github.com>
2020-06-29 16:46:14 +0100

    Add files via upload

---
+++ AButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AButton : MonoBehaviour, IPointerDownHandler
+{
+    public bool pressed = false;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pressed = true;
+        StartCoroutine(ClickTimeOut());
+    }
+
+
+    IEnumerator ClickTimeOut()
+    {
+        yield return new WaitForSeconds(1.0f);
+        pressed = false;
+    }
+}
---
+++ BButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BButton : MonoBehaviour, IPointerClickHandler
+{
+    public bool pressed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }   
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        pressed = true;
+        StartCoroutine(ClickTimeOut());
+    }
+    IEnumerator ClickTimeOut()
+    {
+        yield return new WaitForSeconds(0.1f);
+        pressed = false;
+    }
+}
